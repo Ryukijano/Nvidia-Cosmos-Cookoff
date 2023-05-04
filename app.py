7cddbb23b9a67a9960164be3735c82215b0ec551
Gyanateet Dutta <Ryukijano@users.noreply.huggingface.co>
2023-05-04 19:45:13 +0000

    Update app.py
    Stuff left to implement

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
 import gradio as gr
-import jax
-import numpy as np
 import jax.numpy as jnp
+from diffusers import FlaxStableDiffusionControlNetPipeline, FlaxControlNetModel
+from diffusers import FlaxScoreSdeVeScheduler, FlaxDPMSolverMultistepScheduler
+import torch
+torch.backends.cuda.matmul.allow_tf32 = True
+import torchvision
+import torchvision.transforms as T
 from flax.jax_utils import replicate
 from flax.training.common_utils import shard
+import cv2
+import PIL
 from PIL import Image
-from diffusers import FlaxStableDiffusionControlNetPipeline, FlaxControlNetModel
-from diffusers import UniPCMultistepScheduler
-
+import numpy as np
+import jax
 
 def create_key(seed=0):
     return jax.random.PRNGKey(seed)
@@ -16,38 +21,63 @@ def create_key(seed=0):
 output_res = (768,768)
 
 
-
-def conditioning_image_transforms(image):
-    image = jnp.array(image) / 255.0 #convert the image into JAX array and normalize to [0,1]
-    image = (image - 0.5) * 2 # normalize to [-1, 1]
-    return image
-
-
-controlnet, controlnet_params = FlaxControlNetModel.from_pretrained("./models/catcon-controlnet-wd", dtype=jnp.bfloat16
+conditioning_image_transforms = T.Compose(
+    [
+        #T2.ScaleJitter(target_size=output_res, scale_range=(0.5, 3.0))),
+        T.RandomCrop(size=output_res, pad_if_needed=True, padding_mode="symmetric"),
+        T.ToTensor(),
+        #T.Normalize([0.5], [0.5]),
+    ]
 )
-pipe = FlaxStableDiffusionControlNetPipeline.from_pretrained(
-        "./models/wd-1-5-b2", dtype=jnp.bfloat16, from_pt=True
+
+cnet, cnet_params = FlaxControlNetModel.from_pretrained("Ryukijano/catcon-controlnet-wd", dtype=jnp.bfloat16, from_flax=True)
+pipe, params = FlaxStableDiffusionControlNetPipeline.from_pretrained(
+        "./models/wd-1-5-b2-flax",
+        controlnet=cnet,
+        revision="flax",
+        dtype=jnp.bfloat16,
         )
-pipe.scheduler = UniPCMultistepScheduler.from_config(pipe.scheduler.config)
 
 key = jax.random.PRNGKey(0)
 
 # inference function takes prompt, negative prompt and image
+# inference function takes prompt, negative prompt and image
 def infer(prompt, negative_prompt, image):
-    if seed == 0:
-        seed = jax.random.randint(jax.random.PRNGKey(0), (), 1, 1001)[0] #generate a random seed if slider is set to 0
+    # implement your inference function here
+    params["controlnet"] = cnet_params
+    num_samples = 1
 
-    key = create_key(seed) #using the create_key function created above
+    inp = Image.fromarray(image)
+
+    cond_input = conditioning_image_transforms(inp)
+    cond_input = T.ToPILImage()(cond_input)
+
+    cond_img_in = pipe.prepare_image_inputs([cond_input] * num_samples)
+    cond_img_in = shard(cond_img_in)
+
+    prompt_in = pipe.prepare_text_inputs([prompt] * num_samples)
+    prompt_in = shard(prompt_in)
+
+    n_prompt_in = pipe.prepare_text_inputs([negative_prompt] * num_samples)
+    n_prompt_in = shard(n_prompt_in)
+
+    rng = get_random(0)
+    rng = jax.random.split(rng, jax.device_count())
+
+    p_params = replicate(params)
     
     output = pipe(
-        prompt,
-        image,
-        generator=key, #using the JAX random number generator defined above
-        num_images_per_prompt=1,
-        num_inference_steps=20
-    )
+        prompt_ids=prompt_in,
+        image=cond_img_in,
+        params=p_params,
+        prng_seed=rng,
+        num_inference_steps=70,
+        neg_prompt_ids=n_prompt_in,
+        jit=True,
+            ).images
 
-    return output.images
+    output_images = pipe.numpy_to_pil(np.asarray(output.reshape((num_samples,) + output.shape[-3:])))
+    return output_images
 
 gr.Interface(
     infer,
